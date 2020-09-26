f96540a89360d35a3019300933925b9b942d138c
Abheek Dagar <dagarabheek@gmail.com>
2020-09-26 14:34:38 +0530

    unicafe 1.7 complete

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -29,7 +29,9 @@ const Part = (props) => {
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    <p>
+      Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+    </p>
   )
 }
 
--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -13,15 +13,37 @@ const Display = ({ text, value }) => (
   </div>
 )
 
+const Statistics = ({ good, neutral, bad }) => {
+  let total = good + neutral + bad
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+        <Display text="all" value={total} />
+        <Display text="average" value={0} />
+        <Display text="positive" value={0} />
+      </div>
+    )
+  }
+  else {
+    return (
+      <div>
+        <Display text="all" value={total} />
+        <Display text="average" value={(good - bad) / total} />
+        <Display text="positive" value={((good / total) * 100) + " %"} />
+      </div>
+    )
+  }
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const setValueGood = () => setGood(good + 1)
-  const setValueNeutral = () => setNeutral(neutral + 1)
-  const setValueBad = () => setBad(bad + 1)
+  const setValueGood = () => (setGood(good + 1))
+  const setValueNeutral = () => (setNeutral(neutral + 1))
+  const setValueBad = () => (setBad(bad + 1))
 
   return (
     <div>
@@ -37,6 +59,7 @@ const App = () => {
       <Display text="good" value={good} />
       <Display text="neutral" value={neutral} />
       <Display text="bad" value={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
