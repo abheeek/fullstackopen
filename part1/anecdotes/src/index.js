063e2ca99f5b24941ccfe3d47c712f88dfdcb118
Abheek Dagar <dagarabheek@gmail.com>
2020-09-29 10:44:48 +0530

    anecdotes 1.14 complete

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -8,16 +8,21 @@ const getRandomInt = (min, max) => {
 }
 
 const Button = ({ handleClick, text }) => (
-  <div>
     <button onClick={handleClick}>
       {text}
     </button>
-  </div>
+)
+
+const Display = ({ anecdote, votes }) => (
+  <div>
+    {anecdote}<br/>has {votes} votes
+  </div> 
 )
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [points, setPoints] = useState(new Array(6).fill(0))
+  const [maxPointsIndex, setMaxPointsIndex] = useState(0)
 
   const setValueSelected = () => setSelected(getRandomInt(0, 5))
 
@@ -25,16 +30,23 @@ const App = (props) => {
     const copy = [...points]
     copy[selected] += 1
     setPoints(copy)
+
+    let largest = Math.max(...copy)
+    setMaxPointsIndex(copy.indexOf(largest))
   }
 
   return (
     <div>
-      {props.anecdotes[selected]}
-      <p>
-        has {points[selected]} votes
-      </p>
+      <h1>
+        Anecdote of the day
+      </h1>
+      <Display anecdote={props.anecdotes[selected]} votes={points[selected]} />
       <Button handleClick={setValuePoints} text={"vote"} />
       <Button handleClick={setValueSelected} text={"next anecdote"} />
+      <h1>
+        Anecdote with most votes
+      </h1>
+      <Display anecdote={props.anecdotes[maxPointsIndex]} votes={points[maxPointsIndex]} />
     </div>
   )
 }
